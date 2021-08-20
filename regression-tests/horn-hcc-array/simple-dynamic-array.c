0da178bc86601b661c64e15d633aab19b826c32d
Zafer Esen <zafer.esen@it.uu.se>
2021-08-20 14:19:45 +0200

    added some array programs, wip

---
+++ regression-tests/horn-hcc-array/simple-dynamic-array.c
@@ -0,0 +1,8 @@
+int * a;
+
+void main() {
+  a = malloc(sizeof(int)*(2));
+  a[0] = 1;
+  a[1] = 2;
+  assert((a[0] + a[1]) == 3);
+}
---
+++ regression-tests/horn-hcc-array/simple-global-array.c
@@ -0,0 +1,10 @@
+int a[10];
+
+void main() {
+  //int x;
+  //a[1] = 42;
+  //assert(a[0] == 0);
+  //assert(a[1] == 42);
+  a[0] = 40;
+  assert(a[0] + 2 == 42);
+}
