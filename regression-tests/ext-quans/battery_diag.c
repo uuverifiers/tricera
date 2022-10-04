441d0453f589dd29036b304244df4d3fb8a54af3
Jesper Amilon <jesper.amilon@gmail.com>
2022-10-04 16:44:43 +0200

    battery example

---
+++ regression-tests/ext-quans/battery_diag.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+
+
+typedef struct {
+    int cells[10]; //Voltage value for each cell
+    int max_value;//Maximum voltage value in battery
+    int min_value;
+} BATTERY;
+
+
+void main() {
+BATTERY batt;
+
+  batt.max_value = batt.cells[0];
+  int i = 0;
+  while(i < 10) {
+    if (batt.cells[i] > batt.max_value) {
+      batt.max_value = batt.cells[i];
+    }
+    i++;
+  }
+  assert(batt.max_value == \max(batt.cells, 0, 10));
+}
