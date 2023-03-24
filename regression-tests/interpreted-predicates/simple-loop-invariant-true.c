5c4a9359de196cc94d8a404ba1b092e15094f8d0
Zafer Esen <zafer.esen@it.uu.se>
2023-03-24 11:28:53 +0100

    Add missing regression tests for interpreted predicates.

---
+++ regression-tests/interpreted-predicates/simple-loop-invariant-true.c
@@ -0,0 +1,23 @@
+/*$
+P(int x, int i, int n) {
+  x == i*n && i <= n //0 <= i not needed
+}
+$*/
+
+void main() {
+  int n = _;
+  assume(n > 0);
+  int x = 0;
+  int i = 0;
+  assert(P(x, i, n));
+  while (i < n) {
+    assume(P(x, i, n)); // does not work without this assumption
+    x += n;
+    i++;
+    assert(P(x, i, n));
+  }
+  assume(P(x, i, n));
+
+  
+  assert(x == n*n);
+}
