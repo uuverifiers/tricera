28f71c6cbb3a412551195f8d32afee0bd71ce572
Zafer Esen <zafer.esen@it.uu.se>
2025-11-13 13:20:43 +0100

    Update at expressions so that first argument must be a string containing a label

--- regression-tests/at-expressions/simple-01-false.c
+++ regression-tests/at-expressions/simple-01-false.c
@@ -8,7 +8,7 @@ L1: // x = 0;
 L2:; // x = 42; y = 3;
   x = 3;
   
-  assert($at(L1, (int)(x+5)) == 5);
-  assert($at(L2, (int)(x+y)) == 43);
+  assert($at("L1", (int)(x+5)) == 5);
+  assert($at("L2", (int)(x+y)) == 43);
   assert(x == 3);
 }
